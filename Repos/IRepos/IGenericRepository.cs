32dfd51459e29f0a62356beb2faae6807ea2e7b8
Moithaopi Monwela <mmonwela@botswanapost.co.bw>
2020-11-26 16:27:16 +0200

    update Repo

---
+++ Repos/IRepos/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using RepositoryPattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Repos.IRepos
+{
+    public interface IGenericRepository
+    {
+        Todo GetById(int id);
+        IEnumerable<Todo> GetAll();
+        public void Add(Todo entity);
+        public void Save();
+    }
+}
---
+++ Repos/TodoRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RepositoryPattern.Data;
+using RepositoryPattern.Models;
+using RepositoryPattern.Repos.IRepos;
+
+namespace RepositoryPattern.Repos
+{
+    public class TodoRepository : IGenericRepository
+    {
+        protected readonly ApplicationDbContext _context;
+        public TodoRepository(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public void Add(Todo entity)
+        {
+            _context.Set<Todo>().Add(entity);
+        }
+
+        public IEnumerable<Todo> GetAll()
+        {
+            return _context.Set<Todo>().ToList();
+        }
+
+        public Todo GetById(int id)
+        {
+            return _context.Set<Todo>().Find(id);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
