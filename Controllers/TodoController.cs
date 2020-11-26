5587440425e2a6adad0a7903cf8e404997931fe2
Moithaopi Monwela <mmonwela@botswanapost.co.bw>
2020-11-26 16:22:24 +0200

    add TodoController

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPattern.Repos;
+using RepositoryPattern.Repos.IRepos;
+using RepositoryPattern.Models;
+
+namespace RepositoryPattern.Controllers
+{
+    public class TodoController : Controller
+    {
+        private IGenericRepository _repo;
+        public TodoController(IGenericRepository repo)
+        {
+            _repo = repo;
+        }
+        public IActionResult Index()
+        {
+            //_repo.GetAll();
+            return View(_repo.GetAll());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Add(todo);
+                _repo.Save();
+            }
+
+            return View(todo);
+        }
+    }
+}
